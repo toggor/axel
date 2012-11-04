5ae242635076b441a0f57a4bb6dac54760f28294
Jehy <fate@jehy.ru>
2012-11-04 11:02:23 +0400

    v 0.05
    added formatting (no more fuckup in results) and some error reporting

--- src/com/example/accsel/MainActivity.java
+++ src/com/example/accsel/MainActivity.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -19,32 +22,35 @@ import android.widget.TextView;
 public class MainActivity extends Activity implements SensorEventListener {
 	private SensorManager sensorManager;
 
-
-
 	TextView xCoor; // declare X axis object
 	TextView yCoor; // declare Y axis object
 	TextView zCoor; // declare Z axis object
 	TextView recNum;
 	File dataDir;
 	File saveFile;
-	int state=0;
+	int state = 0;
 	PrintWriter csvWriter;
 	Time now = new Time();
 
 	public void startListen(View view) {
 		Log.v("Axel Listener", "Starting listener");
-		if(state!=0)
-		{
-			Log.e("Start Listener","State!=0, not starting!");
+		if (state != 0) {
+			Log.e("Start Listener", "State!=0, not starting!");
 			return;
 		}
-		state=1;
 		now.setToNow();
 		String TimeStampDB = now.format("%F_%H-%M-%S");
-		
-		int FileNum = dataDir.list().length + 1;
+		int FileNum = 0;
+		try {
+			FileNum = dataDir.list().length + 1;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			showError("Can not get Datadir size!");
+			e.printStackTrace();
+			return;
+		}
 		recNum.setText("Record # : " + FileNum);
-		if(recNum.getVisibility()!=android.view.View.VISIBLE)
+		if (recNum.getVisibility() != android.view.View.VISIBLE)
 			recNum.setVisibility(android.view.View.VISIBLE);
 		saveFile = new File(dataDir.getAbsolutePath() + "/" + FileNum + "_"
 				+ TimeStampDB + ".csv");
@@ -54,6 +60,8 @@ public class MainActivity extends Activity implements SensorEventListener {
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				showError("Can not create new file!");
+				return;
 			}
 
 		try {
@@ -61,29 +69,44 @@ public class MainActivity extends Activity implements SensorEventListener {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			showError("Can not create new writer!");
+			return;
 		}
-		sensorManager.registerListener(this,
-				sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),
-				SensorManager.SENSOR_DELAY_FASTEST);
+		state = 1;
+		sensorManager
+				.registerListener(this, sensorManager
+						.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),
+						SensorManager.SENSOR_DELAY_FASTEST);
+	}
+
+	public void showError(String text) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(text).setPositiveButton("Ok",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+					}
+				});
+
+		builder.show();
 	}
 
 	public void stopListen(View view) {
 		Log.v("Axel Listener", "Stopping listener");
-		if(state!=1)
-		{
-			Log.e("Stop Listener","State!=1, not starting!");
+		if (state != 1) {
+			Log.e("Stop Listener", "State!=1, not stopping!");
 			return;
 		}
-		state=0;
-		sensorManager.unregisterListener(this,
-				sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION));
+		sensorManager
+				.unregisterListener(this, sensorManager
+						.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION));
 		csvWriter.close();
+		state = 0;
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.v("Create","Creating...");
+		Log.v("Create", "Creating...");
 
 		setContentView(R.layout.activity_main);
 
@@ -96,8 +119,13 @@ public class MainActivity extends Activity implements SensorEventListener {
 		String extStorageDirectory = Environment.getExternalStorageDirectory()
 				.getAbsolutePath() + "/axel";
 		dataDir = new File(extStorageDirectory);
-		if (!dataDir.exists())
-			dataDir.mkdir();
+		if (!dataDir.exists()) {
+			try {
+				dataDir.mkdir();
+			} catch (Exception e) {
+				showError("Can not create new Datadir on SD card!");
+			}
+		}
 	}
 
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -113,28 +141,21 @@ public class MainActivity extends Activity implements SensorEventListener {
 			float x = event.values[0];
 			float y = event.values[1];
 			float z = event.values[2];
-
-			try {
-				writeToLog(x, y, z,event.timestamp);
-			} catch (IOException e) {
-				e.printStackTrace();
-				stopListen(null);
-			}
-
+			writeToLog(x, y, z, event.timestamp);
 			xCoor.setText("X: " + x);
 			yCoor.setText("Y: " + y);
 			zCoor.setText("Z: " + z);
 		}
 	}
 
-	public void writeToLog(float x, float y, float z,long nanos) throws IOException {
-
+	public void writeToLog(float x, float y, float z, long nanos)
+	{
 		try {
-			csvWriter.print(x + ";" + y + ";" + z + ";" + nanos	+ "\n");
-
+			csvWriter.printf("%f;%f;%f;%d%s", x, y, z, nanos, "\n");
 		} catch (Exception er) {
 			er.printStackTrace();
 			stopListen(null);
+			showError("Can not print to file");
 		}
 
 	}
